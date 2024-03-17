53831e6c75c719cdb4e3385df962a88a31a5e964
James Clifford <jamesclifford7@gmail.com>
2024-03-17 12:06:13 -0700

    connected to local mysql database

--- app.js
+++ app.js
@@ -8,13 +8,41 @@ const validator = require('email-validator')
 app.use(express.json());
 app.use(cors()); 
 
-const PORT = process.env.PORT || 8000;
+const pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD, 
+    database: process.env.DATABASE, 
+    connectionLimit: 10, 
+    connectTimeout: 60000, 
+});
+  
+pool.getConnection((error)=> {
+    if (error) {
+        console.log('Connection Failed', error.message);
+    } else {
+        console.log('Connection Established Successfully'); 
+    }; 
+});
 
-// Define your routes here
-
+// routes
 app.get('/', (req, res) => {
     res.send('FamilyFi Server is running')
 })
+
+// get all users
+app.get('/users', async (req, res) => {
+    pool.query(`SELECT * FROM ${process.env.DATABASE}.users;`, (error, result) => {
+        if (error) {
+        console.log(error)
+        }
+
+        res.json(result); 
+    })
+})
+
+const PORT = process.env.PORT || 8000;
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
